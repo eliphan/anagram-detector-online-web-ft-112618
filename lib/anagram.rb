5ce80db3166161c3237a0438eed7752cfe7d67b3
Elise Phan <aliceconnic@gmail.com>
2018-12-06 19:36:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  @@all = []
+  def initialize(word)
+    @word = word
+    @@all << word
+  end
+  
+  def match(new_words)
+    new_words = []
+    new_words.each {|new_word| new_words.split("")}
+    @@all.each {|words| words.split("")}
+    
+ 
+   end
+  end
+  
+end
